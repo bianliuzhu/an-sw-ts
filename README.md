c16148f6fad8e218ab8d9f0ff82bfcab2c4f1ddc
Gleason <bianliuzhu@gmail.com>
2023-03-16 21:54:39 +0800

    发布1版本

---
+++ README.md
@@ -0,0 +1,11 @@
+# an-sw-ts
+
+Swagger JSON schema 导出 typescript interface
+
+## 预览
+
+![img](./assets/images/preview.png)
+
+## 快捷键
+
+- 搜索接口列表: <kbd>alt</kbd> + <kbd>shift</kbd> + <kbd>F</kbd>
---
+++ templates/new.template.js
@@ -0,0 +1,8 @@
+/**
+ * This is template config for extension: [Gleason.an-sw-ts]
+ * This is a JavaScript code file that will be executed in the Node environment
+ * And you can add any Javascript(commonjs) code here
+ * For more advanced usage, please see this wiki: https://github.com/Gleason/create-item-by-template/wiki/Template-Example
+ */
+
+module.exports = {}
